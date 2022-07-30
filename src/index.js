829fd277c9ba006b3feffddb8ee77cdff227ec56
nikolapivac <npivac00@fesb.hr>
2022-07-30 17:37:15 +0200

    Synced background image with weather

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import { displayData } from "./displayData";
 const location = document.getElementById("location");
 const submitBtn = document.querySelector(".search_btn");
 
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Makarska&APPID=e87b1d0df402ffef61de75ccb3df3f8a&units=metric`, {mode: "cors"})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        displayData(response);
+        console.log(response);
+    })
+
 submitBtn.addEventListener("click", () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location.value}&APPID=e87b1d0df402ffef61de75ccb3df3f8a&units=metric`, {mode: "cors"})
     .then(function(response) {
